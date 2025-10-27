7a5c9ad21ef28a173c8b01cce621e5e6d429c73b
markpippins <mpippins@gmail.com>
2025-10-27 10:49:04 -0400

    feat: Add block and height styles to TabComponent
    Applies `display: block` and `height: 100%` to the TabComponent host element. This ensures tabs occupy the full available vertical space within their parent container, improving layout consistency.

--- src/components/tabs/tab.component.ts
+++ src/components/tabs/tab.component.ts
@@ -7,6 +7,9 @@ import { Component, ChangeDetectionStrategy, input, signal } from '@angular/core
       <ng-content></ng-content>
     </div>
   `,
+  host: {
+    class: 'block h-full'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabComponent {
